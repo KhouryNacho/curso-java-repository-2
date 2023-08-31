d22ca37127ae19ae78be925385c99d8595699f40
Khoury Nacho <bkcoury@gmail.com>
2023-08-31 01:12:49 -0300

    modulo 5 unidad 4

---
+++ scm_page/backend/routes/admin/login.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var usersModel = require('./../../models/usersModel');
+
+router.get('/', function (req, res, next) {
+    res.render('admin/login', {
+        layout: 'admin/layout'
+    });
+});
+
+
+router.post('/', async (req, res, next) => {
+    try {
+        var user_name = req.body.user_name; //take the info from the input
+        var user_password = req.body.user_password; //password still unencrypted
+
+        var data = await usersModel.getUserByUsernameAndPassword(user_name, user_password);
+
+        if (data != undefined) {
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = router;
